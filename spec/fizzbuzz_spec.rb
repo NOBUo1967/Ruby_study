8fd863db0127f218a0c0b58bfe5eef8a5ff9b301
Shin H <nobunegi13@gmail.com>
2021-04-20 06:50:24 +0900

    fizzbuzzのrspecのテストをリファクタリングした。

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,24 +1,29 @@
 require 'spec_helper'
 
 RSpec.describe 'fizz_buzz' do
+  subject { fizz_buzz(num) }
   context '数字が15の倍数の時' do
+    let(:num) { 15 }
     it 'Fizz Buzzと返す' do
-      expect(fizz_buzz(15)).to eq 'Fizz Buzz'
+      is_expected.to eq 'Fizz Buzz'
     end
   end
   context '数字が3の倍数の時' do
+    let(:num) { 3 }
     it 'Fizzと返す' do
-      expect(fizz_buzz(3)).to eq 'Fizz'
+      is_expected.to eq 'Fizz'
     end
   end
   context '数字が5の倍数の時' do
+    let(:num) { 5 }
     it 'Buzzと返す' do
-      expect(fizz_buzz(5)).to eq 'Buzz'
+      is_expected.to eq 'Buzz'
     end
   end
   context '数字が3,5,15のいずれの倍数ではない時' do
+    let(:num) { 1 }
     it '数字の文字列を返す' do
-      expect(fizz_buzz(1)).to eq '1'
+      is_expected.to eq '1'
     end
   end
 end
