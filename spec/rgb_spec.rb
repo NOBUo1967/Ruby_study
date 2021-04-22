b8045df1c08e37011570a29065a736fc8a94e5b7
Shin H <nobunegi13@gmail.com>
2021-04-23 06:34:33 +0900

    別のパターンでto_hexメソッドが通るかテストした。

--- spec/rgb_spec.rb
+++ spec/rgb_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 RSpec.describe 'RGB_test' do
-  context 'test_to_hex' do
-    it '(0, 0, 0)は#000000を返す' do
-      expect(to_hex(0, 0, 0)).to eq '#000000'
+  describe'test_to_hex' do
+    context 'to_hex(0, 0, 0)の時' do
+      it '#000000を返す' do
+        expect(to_hex(0, 0, 0)).to eq '#000000'
+      end
+    end
+
+    context 'to_hex(255, 255, 255)の時' do
+      it '#ffffffを返す' do
+        expect(to_hex(255, 255, 255)).to eq '#ffffff'
+      end
     end
   end
 end
