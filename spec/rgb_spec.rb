778a573a14e484941c2c992c3e820fb606852e3b
Shin H <nobunegi13@gmail.com>
2021-04-23 06:55:09 +0900

    [test] 16進数から10進数に変換するto_intsメソッドのテストを定義した。

--- spec/rgb_spec.rb
+++ spec/rgb_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe 'RGB_test' do
-  describe'test_to_hex' do
+  describe 'test_to_hex' do
     context 'to_hex(0, 0, 0)の時' do
       it '#000000を返す' do
         expect(to_hex(0, 0, 0)).to eq '#000000'
@@ -17,6 +17,14 @@ RSpec.describe 'RGB_test' do
     context 'to_hex(4, 60, 120)の時' do
       it '#043c78を返す' do
         expect(to_hex(4, 60, 120)).to eq '#043c78'
+      end
+    end
+  end
+
+  describe 'test_to_ints' do
+    context "to_ints('#000000')の時" do
+      it '[0, 0, 0]を返す' do
+        expect(to_ints('#000000')).to eq [0, 0, 0]
       end
     end
   end
