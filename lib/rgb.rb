0823f974ad7e8d00da87fe346b58f959624602c1
Shin H <nobunegi13@gmail.com>
2021-04-24 20:13:00 +0900

    [chore]pry-byebugを導入した。

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def to_hex(r, g, b)
   [r, g, b].reduce('#') do |hex, color|
     hex + color.to_s(16).rjust(2, '0')
@@ -5,5 +7,8 @@ def to_hex(r, g, b)
 end
 
 def to_ints(hex)
+  binding.pry
   hex.scan(/\w\w/).map(&:hex)
 end
+
+to_ints(ARGV[0])
