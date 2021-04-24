230ad6755a3dea0063eabb0f1c4c9ba3a46ee911
Shin H <nobunegi13@gmail.com>
2021-04-25 07:31:12 +0900

    [refactor]引数にキーワード引数を使用。デフォルト値を設定。UNITSハッシュを定数化した。

--- spec/convert_length_spec.rb
+++ spec/convert_length_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 RSpec.describe 'ConvertLengthTest' do
   describe 'test_convert_length' do
-    context "convert_length(1, 'm', 'in')の時" do
+    context "convert_length(1, from: :m, to: :in)の時" do
       it '39.37を返す' do
-        expect(convert_length(1, 'm', 'in')).to eq 39.37
+        expect(convert_length(1, from: :m, to: :in)).to eq 39.37
       end
     end
 
-    context "convert_length(15, 'in', 'm')の時" do
+    context "convert_length(15, from: :in, to: :m)の時" do
       it '0.38を返す' do
-        expect(convert_length(15, 'in', 'm')).to eq 0.38
+        expect(convert_length(15, from: :in, to: :m)).to eq 0.38
       end
     end
 
-    context "convert_length(35000, 'ft', 'm')の時" do
+    context "convert_length(35000, from: :ft, to: :m)の時" do
       it '10670.73を返す' do
-        expect(convert_length(35000, 'ft', 'm')).to eq 10670.73
+        expect(convert_length(35000, from: :ft, to: :m)).to eq 10670.73
       end
     end
   end
