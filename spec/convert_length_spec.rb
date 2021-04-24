2ee83d000bea7ae3f4597140fec955bb76afa982
Shin H <nobunegi13@gmail.com>
2021-04-24 22:42:59 +0900

    [test]inchからmへの変換のテストを定義した。

--- spec/convert_length_spec.rb
+++ spec/convert_length_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe 'ConvertLengthTest' do
         expect(convert_length(1, 'm', 'in')).to eq 39.37
       end
     end
+
+    context "convert_length(15, 'in', 'm')の時" do
+      it '0.38を返す' do
+        expect(convert_length(15, 'in', 'm')).to eq 0.83
+      end
+    end
   end
 end
