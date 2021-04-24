84589341c74a2a61930bbe8e7a2853ff539175cc
Shin H <nobunegi13@gmail.com>
2021-04-25 07:02:28 +0900

    [test]ftからmへの変換のテストを定義した。inchからmへの変換の期待する値が間違っていたため修正した。

--- spec/convert_length_spec.rb
+++ spec/convert_length_spec.rb
@@ -10,7 +10,13 @@ RSpec.describe 'ConvertLengthTest' do
 
     context "convert_length(15, 'in', 'm')の時" do
       it '0.38を返す' do
-        expect(convert_length(15, 'in', 'm')).to eq 0.83
+        expect(convert_length(15, 'in', 'm')).to eq 0.38
+      end
+    end
+
+    context "convert_length(35000, 'ft', 'm')の時" do
+      it '10670.73を返す' do
+        expect(convert_length(35000, 'ft', 'm')).to eq 10670.73
       end
     end
   end
