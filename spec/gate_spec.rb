1eba05556cc7e1a97883e5355a2f6cad13182bf0
Shin H <nobunegi13@gmail.com>
2021-04-25 16:18:35 +0900

    [refactor]テストコードのリファクタリングを行った。
    各describeで作成していたローカル変数をインスタンス変数に格納した。

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -1,24 +1,26 @@
 require 'spec_helper'
 
 RSpec.describe Gate do
-  describe 'gateクラス' do
+  describe '改札機プログラム' do
+    before do
+      @umeda = Gate.new(:umeda)
+      @juso = Gate.new(:juso)
+      @mikuni = Gate.new(:mikuni)
+    end
+
     context '150円のきっぷを購入し、梅田で入場し、十三で出場しようとした時' do
       it '出場できる' do
-        umeda = Gate.new(:umeda)
-        juso = Gate.new(:juso)
         ticket = Ticket.new(150)
-        umeda.enter(ticket)
-        expect(juso.exit(ticket)).to eq true
+        @umeda.enter(ticket)
+        expect(@juso.exit(ticket)).to eq true
       end
     end
 
     context '150円のきっぷを購入し、梅田で入場し、三国で出場しようとした時' do
       it '出場できない' do
-        umeda = Gate.new(:umeda)
-        mikuni = Gate.new(:mikuni)
         ticket = Ticket.new(150)
-        umeda.enter(ticket)
-        expect(mikuni.exit(ticket)).to eq false
+        @umeda.enter(ticket)
+        expect(@mikuni.exit(ticket)).to eq false
       end
     end
   end
