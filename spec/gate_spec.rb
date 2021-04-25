374177f30b5788e7d18f75d1b803cb61df2a961a
Shin H <nobunegi13@gmail.com>
2021-04-25 15:52:50 +0900

    [test]出場できないパターンのテストを定義した。

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe Gate do
-  describe '#new' do
+  describe 'gateクラス' do
     context '150円のきっぷを購入し、梅田で入場し、十三で出場しようとした時' do
       it '出場できる' do
         umeda = Gate.new(:umeda)
@@ -9,6 +9,16 @@ RSpec.describe Gate do
         ticket = Ticket.new(150)
         umeda.enter(ticket)
         expect(juso.exit(ticket)).to eq true
+      end
+    end
+
+    context '150円のきっぷを購入し、梅田で入場し、三国で出場しようとした時' do
+      it '出場できない' do
+        umeda = Gate.new(:umeda)
+        mikuni = Gate.new(:mikuni)
+        ticket = Ticket.new(150)
+        umeda.enter(ticket)
+        expect(mikuni.exit(ticket)).to eq false
       end
     end
   end
