e45204143433d0bc723eea7d6f62390da9b69a21
Shin H <nobunegi13@gmail.com>
2021-04-25 11:47:16 +0900

    [test]gateテストの内容を詳細に定義した。

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 
 RSpec.describe Gate do
   describe '#new' do
-    Gate.new
+    context '150円のきっぷを購入し、梅田で入場し、十三で出場しようとした時' do
+      it '出場できる' do
+        umeda = Gate.new(:umeda)
+        juso = Gate.new(:juso)
+        ticket = Ticket.new(150)
+        umeda.enter(ticket)
+        expect(juso.exit(ticket)).to eq true
+      end
+    end
   end
 end
