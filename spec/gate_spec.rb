cb2490f8d3d73dc4e16f215e4c8432d54ac997dc
Shin H <nobunegi13@gmail.com>
2021-04-25 16:22:25 +0900

    [test]各パターンが網羅できるようにテストコードを定義した。

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -23,5 +23,21 @@ RSpec.describe Gate do
         expect(@mikuni.exit(ticket)).to eq false
       end
     end
+
+    context '190円のきっぷを購入し、梅田で入場し、三国で出場しようとした時' do
+      it '出場できる' do
+        ticket = Ticket.new(190)
+        @umeda.enter(ticket)
+        expect(@mikuni.exit(ticket)).to eq true
+      end
+    end
+
+    context '150円のきっぷを購入し、十三で入場し、三国で出場しようとした時' do
+      it '出場できる' do
+        ticket = Ticket.new(150)
+        @juso.enter(ticket)
+        expect(@mikuni.exit(ticket)).to eq true
+      end
+    end
   end
 end
