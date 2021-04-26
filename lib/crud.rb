9f3bcf70b3943ddd2e0f2dd20f9d766fc177f567
Shin H <nobunegi13@gmail.com>
2021-04-26 21:50:30 +0900

    [feat]updateの機能を実装した。

--- lib/crud.rb
+++ lib/crud.rb
@@ -23,6 +23,13 @@ case choose
     else
       puts 'すでに登録されています'
     end
+  when 'update'
+    puts '勝数を更新したい球団を選んでください'
+    team = gets.chomp
+    puts '勝数を入力してください'
+    number_of_win = gets.chomp
+    baseball_team[team.to_sym] = number_of_win.to_i
+    puts "#{team}の勝数が#{number_of_win}に更新されました"
 end #case文のend
 
 puts baseball_team
