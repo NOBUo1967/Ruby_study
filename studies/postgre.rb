6e82a698025ecea4335c3c8b78493eb7c2241772
Shin H <nobunegi13@gmail.com>
2021-04-29 15:42:21 +0900

    [feat]コマンドラインからデータ全件取得やデータ追加などのSQLに対する操作を選択できるようにした。

--- studies/postgre.rb
+++ studies/postgre.rb
@@ -1,16 +1,30 @@
 require 'pg'
 
 begin
-  puts '追加したい球団を入力してください'
-  team_name = gets.chomp
-  puts '球団ナンバーを入力してください'
-  team_no = gets.chomp
   conn = PG.connect('localhost', 5432, '', '', 'testdb', 'SHIN', '')
-  # q = 'SELECT * FROM team'
-  q = "INSERT INTO team VALUES(#{team_no.to_i}, #{team_name})"
-  res = conn.exec(q)
-  res.each do |r|
-    puts r
+
+  puts '====================================='
+  puts '実行したい操作を入力してください'
+  puts '(index), (create)'
+  puts '====================================='
+  choose = gets.chomp
+
+  case choose
+  when 'index'
+    q = 'SELECT * FROM team'
+    res = conn.exec(q)
+    res.each do |r|
+      puts r
+    end
+
+  when 'create'
+    puts '追加したい球団を入力してください'
+    team_name = gets.chomp
+    puts '球団ナンバーを入力してください'
+    team_no = gets.chomp
+    q = "INSERT INTO team VALUES(#{team_no.to_i}, '#{team_name}')"
+    res = conn.exec(q)
+    puts '登録に成功しました'
   end
 rescue => ex
   print(ex.class, '->', ex.message)
