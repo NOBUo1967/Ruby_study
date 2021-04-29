1a348f5f8caaf25b3bcee2101c31fc4273c035ba
Shin H <nobunegi13@gmail.com>
2021-04-29 17:59:56 +0900

    [study]RubyからSQLを操作してデータを更新できるようにした。

--- studies/postgre.rb
+++ studies/postgre.rb
@@ -5,7 +5,7 @@ begin
 
   puts '====================================='
   puts '実行したい操作を入力してください'
-  puts '(index), (create), (delete)'
+  puts '(index), (create), (update), (delete)'
   puts '====================================='
   choose = gets.chomp
 
@@ -23,6 +23,15 @@ begin
     q = "INSERT INTO baseball_team (team_name) VALUES('#{team_name}')"
     conn.exec(q)
     puts '登録に成功しました'
+
+  when 'update'
+    puts '球団名を変更したい球団を入力してください'
+    team_name_before_the_change = gets.chomp
+    puts '変更後の球団名を入力してください'
+    team_name_after_the_change = gets.chomp
+    q = "UPDATE baseball_team SET team_name  = '#{team_name_after_the_change}' WHERE team_name = '#{team_name_before_the_change}'"
+    conn.exec(q)
+    puts '更新が完了しました'
 
   when 'delete'
     puts '削除したい球団の球団名を入力してください'
