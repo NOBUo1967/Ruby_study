80c406233a421194092ed762d40e42c3d9760117
Shin H <nobunegi13@gmail.com>
2021-04-29 15:31:52 +0900

    [study]Rubyを使用してテーブルにデータを追加できるようにした。

--- studies/postgre.rb
+++ studies/postgre.rb
@@ -1,8 +1,13 @@
 require 'pg'
 
 begin
+  puts '追加したい球団を入力してください'
+  team_name = gets.chomp
+  puts '球団ナンバーを入力してください'
+  team_no = gets.chomp
   conn = PG.connect('localhost', 5432, '', '', 'testdb', 'SHIN', '')
-  q = 'SELECT * FROM team'
+  # q = 'SELECT * FROM team'
+  q = "INSERT INTO team VALUES(#{team_no.to_i}, #{team_name})"
   res = conn.exec(q)
   res.each do |r|
     puts r
