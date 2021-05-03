9ff34d6b058e36396e9ed3c703aa6d34186a26d0
Shin H <nobunegi13@gmail.com>
2021-05-03 22:04:19 +0900

    [study]既存の配列をもとに新しい配列を作成。
    map(= collect)はもとの配列に変更を加えずに新たな配列を作成する。
    map!とすると破壊的に配列に変更を加える。

--- studies/array_study.rb
+++ studies/array_study.rb
@@ -1,6 +1,14 @@
 #配列の作成
 a = []
 100.times { |i| a[i] = i + 1}
-a1 = (1..100).to_a
-puts a
-puts a1
+# a1 = (1..100).to_a
+# puts a
+# puts a1
+
+# もとの配列に変更を加えずに新しい配列を作る。
+a2 = a.map { |i| i * 100 }
+puts a2
+
+# もとの配列を破壊的に変更する。
+# a.map! { |i| i * 100 }
+# puts a
