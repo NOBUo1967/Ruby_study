10e9907546f16c5e7348d12aa65f51d9c5041853
Shin H <nobunegi13@gmail.com>
2021-05-08 12:34:57 +0900

    [study] RubyからMySQLにアクセスし、データを追加する処理をした。

--- studies/mysql_test.rb
+++ studies/mysql_test.rb
@@ -5,9 +5,12 @@ Dotenv.load
 # DBに接続
 client = Mysql2::Client.new(host: "localhost", username: "root", password: ENV['MYSQL_PASSWORD'], :encoding => 'utf8', database: 'test_db')
 
+#データの追加
+client.query("INSERT INTO product(id, name, `from`, price) VALUES (2, 'chair', 'japan', 10000)")
 
 # データを全件取得
 product_results = client.query("SELECT * FROM product")
+
 
 # 取得したデータを表示
 product_results.each do |product_result|
